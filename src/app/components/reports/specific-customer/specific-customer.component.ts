c588cc22c1b13cfecb3ae92d29a432f43f78f1b7
hala1mo <halamontaser13@gmail.com>
2023-09-11 12:43:49 +0300

    show forms

---
+++ src/app/components/reports/specific-customer/specific-customer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-specific-customer',
+  templateUrl: './specific-customer.component.html',
+  styleUrls: ['./specific-customer.component.css']
+})
+export class SpecificCustomerComponent {
+
+}
